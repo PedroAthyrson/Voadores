be7cf2d8097e37f5da7f1e8ffb05e9f97f930e3e
Pedro Athyrson <pedroathyrsondev@gmail.com>
2023-04-28 22:12:19 -0300

    finalizando o main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RepositorioDeVoadores voadores = new RepositorioDeVoadores();
+
+        Random r = new Random();
+        for (int i = 0; i < 10 ; i++) {
+            int escolhaAleatoria = r.nextInt(3);
+            switch (escolhaAleatoria){
+                case 0:
+                    System.out.println("\nGalinha!");
+                    voadores.adicionaVoador(new GalinhaVoadora());
+                    break;
+                case 1:
+                    System.out.println("\nDrone!");
+                    voadores.adicionaVoador(new Drone("Tipo Premium"));
+                    break;
+                case 2:
+                    System.out.println("\nUrubu!");
+                    voadores.adicionaVoador(new Urubu());
+                    break;
+            }
+            voadores.fazVoar(i); //chamada polimórfica
+            voadores.fazPlanar(i); //chamada polimórfica
+            voadores.fazPousar(i); //chamada polimórfica
+
+        }
+
     }
 }
